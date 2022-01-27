db2ae99c4ef84053c4af072cf303e6cf8a1b1e3e
ambiguousname <tyly04@gmail.com>
2022-01-27 14:44:58 -0800

    Started writing stuff for settings

---
+++ ProgrammingPlaysCeleste/ProgrammingPlaysCelesteSettings.cs
@@ -0,0 +1,25 @@
+﻿using Celeste;
+using Celeste.Mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Celeste.TextMenu;
+
+namespace ProgrammingPlaysCeleste
+{
+    [SettingName("modoptions_programmingplaysceleste_title")]
+    class ProgrammingPlaysCelesteSettings : EverestModuleSettings
+    {
+        public int NumberOfInputDivisions { get; set; } = 2;
+        public void CreateInputDivisionEntry(TextMenu menu, bool inGame) {
+            Option<string> divisions = new Option<string>("Input Divisions");
+            for (int i = 0; i < NumberOfInputDivisions; i++) {
+                divisions.Add("Inputs allowed for #" + i, "LRUDJCZ");
+                divisions.Add("Folder name for #" + i, i.ToString());
+            }
+            menu.Add(divisions);
+        }
+    }
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+import glob
+
+# Hack-y way around this. Not sure how to communicate directly from C# to python, so we'll just use console input.
+while (1):
+    json = input()
+    print("TEST")
