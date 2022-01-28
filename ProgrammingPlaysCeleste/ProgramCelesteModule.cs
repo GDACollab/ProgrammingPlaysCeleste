693e832f9bf87d86ced5a7ac609db39bdefacf87
ambiguousname <tyly04@gmail.com>
2022-01-28 12:54:32 -0800

    Made some settings menu additions and fixes

--- ProgrammingPlaysCeleste/ProgramCelesteModule.cs
+++ ProgrammingPlaysCeleste/ProgramCelesteModule.cs
@@ -5,6 +5,7 @@ using Celeste.Mod;
 using Microsoft.Xna.Framework;
 using Monocle;
 using System;
+using Celeste.Mod.UI;
 
 namespace ProgrammingPlaysCeleste
 {
@@ -27,24 +28,60 @@ namespace ProgrammingPlaysCeleste
         }
 
         public override Type SettingsType => typeof(ProgramCelesteModuleSettings);
-        public static ProgramCelesteModuleSettings Settings => (ProgramCelesteModuleSettings) Instance._Settings;
+        public static ProgramCelesteModuleSettings Settings => ProgramCelesteModuleSettings.Instance;
 
         static Process movementScripts;
 
         HashSet<Inputs> activeInputs;
 
+        private void DrawInputDivisions(TextMenu menu, ref TextMenu.Item[] inputDivisions) {
+            for (int i = 0; i < 10; i++)
+            {
+                int number = i;
+                TextMenu.Item inputsAllowed = new TextMenu.Button("Inputs allowed for #" + number + ": " + Settings.InputDivisions[number]).Pressed(
+                    () => {
+                        menu.SceneAs<Overworld>().Goto<OuiModOptionString>().Init<OuiModOptions>(Settings.InputDivisions[number], value => Settings.InputDivisions[number] = value);
+                    });
+                TextMenu.Item folderName = new TextMenu.Button("Folder name for #" + number + ": " + Settings.InputFolderNames[number]).Pressed(
+                    () => {
+                        menu.SceneAs<Overworld>().Goto<OuiModOptionString>().Init<OuiModOptions>(Settings.InputFolderNames[number], value => Settings.InputFolderNames[number] = value);
+                    });
+                menu.Add(inputsAllowed);
+                menu.Add(folderName);
+                if (i >= Settings.NumberOfInputDivisions) {
+                    inputsAllowed.Visible = false;
+                    folderName.Visible = false;
+                }
+
+                inputDivisions[i * 2] = inputsAllowed;
+                inputDivisions[(i * 2) + 1] = folderName;
+            }
+        }
+
+        private void UpdateInputDivisions(int num, ref TextMenu.Item[] inputDivisions) {
+            for (int i = 0; i < inputDivisions.Length; i++) {
+                inputDivisions[i].Visible = (i < (num * 2));
+            }
+        }
+
         // Because for some reason I can't use Create[PropName]Entry in CelesteModuleSettings.cs, we're using this hacky workaround:
         public override void CreateModMenuSection(TextMenu menu, bool inGame, FMOD.Studio.EventInstance snapshot)
         {
             base.CreateModMenuSection(menu, inGame, snapshot);
-            ProgramCelesteModuleSettings settings = ProgramCelesteModuleSettings.Instance;
-            for (int i = 0; i < settings.NumberOfInputDivisions; i++)
-            {
-                TextMenu.Setting inputsAllowed = new TextMenu.Setting("Inputs allowed for #" + i, "LRUDJCZ");
-                TextMenu.Setting folderName = new TextMenu.Setting("Folder name for #" + i, i.ToString());
-                menu.Add(inputsAllowed);
-                menu.Add(folderName);
-            }
+
+            TextMenu.Slider numDivisions = new TextMenu.Slider("Number of Input Divisions", (number) => { return number.ToString(); }, 1, 10);
+            numDivisions.Index = Settings.NumberOfInputDivisions;
+            menu.Add(numDivisions);
+
+            // 2 buttons * 10 total input divisions.
+            TextMenu.Item[] inputDivisions = new TextMenu.Item[20];
+
+            DrawInputDivisions(menu, ref inputDivisions);
+
+            numDivisions.Change((value)=> {
+                Settings.NumberOfInputDivisions = value;
+                UpdateInputDivisions(value, ref inputDivisions);
+            });
         }
 
         public override void Load() {
@@ -101,7 +138,7 @@ namespace ProgrammingPlaysCeleste
                     case 'C':
                         activeInputs.Add(Inputs.Climb);
                         break;
-                    case 'Z':
+                    case 'X':
                         activeInputs.Add(Inputs.Dash);
                         break;
                     default:
--- ProgrammingPlaysCeleste/ProgramCelesteModuleSettings.cs
+++ ProgrammingPlaysCeleste/ProgramCelesteModuleSettings.cs
@@ -10,14 +10,19 @@ namespace ProgrammingPlaysCeleste
 {
     public class ProgramCelesteModuleSettings : EverestModuleSettings
     {
-        public static ProgramCelesteModuleSettings Instance;
+        public static ProgramCelesteModuleSettings Instance { get; private set; }
 
-        ProgramCelesteModuleSettings() {
+        public ProgramCelesteModuleSettings() {
             Instance = this;
         }
 
-        [SettingRange(1, 10)]
+        [SettingIgnore]
         public int NumberOfInputDivisions { get; set; } = 2;
 
+        [SettingIgnore] //Because we don't want the array to be dynamically resized for some reason or another (we'd lose settings progress!), we just initialize it with everything we need:
+        public string[] InputDivisions { get; set; } = { "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX", "LRUDJCX" };
+
+        [SettingIgnore] // Same with folder names:
+        public string[] InputFolderNames { get; set; } = { "head", "feet", "face", "neck", "stomach", "waist", "knee", "hand", "finger", "arm" };
     }
 }
